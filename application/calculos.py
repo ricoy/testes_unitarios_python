969f701dfb15c695926e36c73828a5ed5097dcc4
rodrigomarques <rodrigomarques@pbh.gov.br>
2021-10-31 21:34:59 -0300

    adicionado a classe de impressao

--- application/calculos.py
+++ application/calculos.py
@@ -5,18 +5,35 @@ class Calculadora:
     OP_DIVISAO = 'div'
     OP_MULTIPLICACAO = 'multi'
 
-    def __init__(self, num_1, num_2):
-        self.num_1 = num_1
-        self.num_2 = num_2
-
-    def calcular(self, operacao):
+    def calcular(self, num_1, num_2, operacao):
         if (operacao is self.OP_SOMA):
-            return self.num_1 + self.num_2
+            return num_1 + num_2
         elif (operacao is self.OP_SUBTRACAO):
-            return self.num_1 - self.num_2
+            return num_1 - num_2
         elif (operacao is self.OP_DIVISAO):
-            return self.num_1/self.num_2
+            return num_1/num_2
         elif (operacao is self.OP_MULTIPLICACAO):
-            return self.num_1 * self.num_2
+            return num_1 * num_2
         
         raise ValueError('Operacao nao permitida')
+
+class ImpressaoCalculos:
+    def __init__(self, calculadora):
+        self.calculadora = calculadora
+    
+    def obter_calculos(self):
+        return [
+            (1, 1, Calculadora.OP_SOMA),
+            (5, 2, Calculadora.OP_MULTIPLICACAO),
+            (10, 2, Calculadora.OP_DIVISAO)
+        ]
+
+    def imprimir(self):
+        for calculo in self.obter_calculos():
+            print(self.calculadora.calcular(*calculo))
+
+if __name__ == '__main__':
+    calculadora = Calculadora()
+    print(calculadora.calcular(5, 3, Calculadora.OP_MULTIPLICACAO))
+    # imp = ImpressaoCalculos(calculadora)
+    # imp.imprimir()
--- tests/calculos_test.py
+++ tests/calculos_test.py
@@ -1,18 +1,24 @@
 from unittest import TestCase
-from application.calculos import Calculadora
+from application.calculos import Calculadora, ImpressaoCalculos
 
 class TestCalculadora(TestCase):
+    
     def test_calculos(self):
-        calculadora = Calculadora(1, 1)
+        calculadora = Calculadora()
         with self.assertRaises(ValueError):
-            calculadora.calcular(1)
+            calculadora.calcular(1, 1, 'dafdadfa')
+        self.assertEqual(2, calculadora.calcular(1, 1, Calculadora.OP_SOMA))
+        self.assertEqual(0, calculadora.calcular(1, 1, Calculadora.OP_SUBTRACAO))
+        self.assertEqual(1, calculadora.calcular(1, 1, Calculadora.OP_MULTIPLICACAO))
+        self.assertEqual(1, calculadora.calcular(1, 1, Calculadora.OP_DIVISAO))
 
-        with not self.assertRaises(ValueError):
-            self.assertEqual(2, calculadora.calcular(Calculadora.OP_SOMA))
-            
-        self.assertEqual(2, calculadora.calcular(Calculadora.OP_SOMA))
-        self.assertEqual(0, calculadora.calcular(Calculadora.OP_SUBTRACAO))
-        self.assertEqual(1, calculadora.calcular(Calculadora.OP_MULTIPLICACAO))
-        self.assertEqual(1, calculadora.calcular(Calculadora.OP_DIVISAO))
+class TestImpressaoCalculos(TestCase):
+    
+    def test_impressao_calculos(self):
+        imp = ImpressaoCalculos(Calculadora())
+        calculos = imp.obter_calculos()
+        self.assertIsInstance(calculos, list)
+
+      
         
 
